9a237c20d816226e88b54df351ff89618ad40217
Thibaud <thibaud.marti@orange.fr>
2021-12-29 08:27:23 +0100

    Add route and controller likeSauce

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -19,6 +19,52 @@ exports.createSauce = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
+exports.likeSauce = (req, res, next) => {
+  //récupération de l userID ds le corps de la requête
+  const userId = req.body.userId;
+
+  //récupération de l'état du like (-1, 0, 1) ds le corps de la requête
+  const likeState = req.body.like;
+
+  //recherche de la Sauce concernée par l id du paramètre de la requête
+  Sauce.findOne({ _id: req.params.id })
+    .then((result) => {
+      const sauce = result;
+
+      //création de const pour stocker l'index de l'userId ds les tab(usersLiked ou usersDisliked)
+      const indexLike = sauce.usersLiked.indexOf(userId);
+      const indexDislike = sauce.usersDisliked.indexOf(userId);
+
+      //si l'index est existant dans un des deux tableaux on supprime pour éviter les doublons
+      if (indexLike > -1) {
+        sauce.usersLiked.splice(indexLike, 1);
+      }
+      if (indexDislike > -1) {
+        sauce.usersDisliked.splice(indexDislike, 1);
+      }
+
+      //si like = 1 on ajoute userID au tab usersLiked
+      if (likeState === 1) {
+        sauce.usersLiked.push(userId);
+      }
+
+      //si like = -1 on ajoute userID au tab usersDisliked
+      if (likeState === -1) {
+        sauce.usersDisliked.push(userId);
+      }
+
+      // le comptage des like et dislike est équivalent au nombre d'éléments du tab du même nom
+      sauce.likes = sauce.usersLiked.length;
+      sauce.dislikes = sauce.usersDisliked.length;
+
+      //mise à jour de la Sauce avec modifications like
+      Sauce.updateOne({ _id: req.params.id }, sauce)
+        .then(() => res.status(200).json({ message: "likeSauce modifiée" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
+
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file
     ? {
--- routes/sauces.js
+++ routes/sauces.js
@@ -6,6 +6,7 @@ const multer = require("../middleware/multer-config");
 
 router.post("/", auth, multer, saucesCtrl.createSauce);
 router.put("/:id", auth, multer, saucesCtrl.modifySauce);
+router.post("/:id/like", auth, saucesCtrl.likeSauce);
 router.delete("/:id", auth, saucesCtrl.deleteSauce);
 router.get("/:id", auth, saucesCtrl.getOneSauce);
 router.get("/", auth, saucesCtrl.getAllSauce);
